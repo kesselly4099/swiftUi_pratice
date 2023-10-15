58289ab2fdbc21bd4a2941fe34c81fb720ec5958
kesselly kamara <kkesselly30@gmail.com>
2023-10-14 22:52:54 -0700

    Initial Commit

---
+++ SwiftUi_frames_and_layout/SwiftUi_frames_and_layoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_frames_and_layoutApp.swift
+//  SwiftUi_frames_and_layout
+//
+//  Created by k-square001 on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_frames_and_layoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
