2bbe50d4e14d43e12164016dfa1198ec125048e2
kesselly kamara <kkesselly30@gmail.com>
2023-10-14 23:50:04 -0700

    try1

--- SwiftUi_frames_and_layout/ContentView.swift
+++ SwiftUi_frames_and_layout/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        V Stack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
